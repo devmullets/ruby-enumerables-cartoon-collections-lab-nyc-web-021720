ecddf46aa789616fefeaf9bfb2927d14a8853e17
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-11 14:58:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ end
 def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find { |cheese_found| cheese_types.inclue?
+  array.find { |cheese_found| cheese_types.inclue?(cheese_found) }
   
 end
